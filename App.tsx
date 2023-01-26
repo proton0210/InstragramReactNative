7fb391c8629961b2d3303c29bc3772c2c5ef1d47
Vidit Shah <vidit0210@protonmail.com>
2023-01-27 03:34:58 +0530

    setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import colors from "./src/theme/colors";
+import fonts from "./src/theme/fonts";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ color: colors.primary, fontSize: fonts.size.lg }}>
+        Hello from Expo!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
